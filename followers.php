3f00bbd7752ee6c934dc088716b2a743b60aa8db
Toph Tucker <tophtucker@gmail.com>
2013-11-05 00:33:44 -0500

    It's working! Fetch all alums from both lists and display sorted from most followers to fewest

--- followers.php
+++ followers.php
@@ -13,8 +13,11 @@ function getConnectionWithAccessToken($oauth_token, $oauth_token_secret) {
 $connection = getConnectionWithAccessToken("5056501-ujO2WnCuxruqoDbUxPKzcEybDdSEJh5G39kBGkbj2F", "4SJgrmGwbTObWiUQwCPoaRZwAfJceFeg2kqWW1jPawjDU");
 
 //$content = $connection->get("lists/members.json?slug=alumni-students-1&owner_screen_name=bowdoincollege&cursor=-1");
+//$users = $content->users;
 
 $users = array();
+
+// load everything from first bowdoin alumni list
 $next_cursor = -1;
 $i = 0;
 while($next_cursor !== 0) {
@@ -23,6 +26,28 @@ while($next_cursor !== 0) {
 	$users = array_merge($users, $page->users);
 	$i++;
 }
+
+// load everything from second bowdoin alumni list
+$next_cursor = -1;
+$i = 0;
+while($next_cursor !== 0) {
+	$page = $connection->get("lists/members.json?slug=alumni-students-2&owner_screen_name=bowdoincollege&cursor=".$next_cursor);
+	$next_cursor = $page->next_cursor;
+	$users = array_merge($users, $page->users);
+	$i++;
+}
+
+// compare follower counts to sort highest to lowest
+function cmp_followers($a, $b)
+{
+	if ($a->followers_count == $b->followers_count) {
+        return 0;
+    }
+    return ($a->followers_count > $b->followers_count) ? -1 : 1;
+}
+
+// sort users from highest follower count to lowest
+usort($users, "cmp_followers");
 
 ?>
 
@@ -34,15 +59,17 @@ while($next_cursor !== 0) {
 </head>
 <body>
 
-<ul>
+<h1>Bowdoin alumni with most followers</h1>
+
+<ol>
 <? 
-//print_r($content);
+//print_r($users);
 foreach($users as $user) { 
 	//print_r($user);
 	print "<li><b>".$user->screen_name."</b> - ".$user->followers_count."</li>";
 }
 ?>
-</ul>
+</ol>
 
 </body>
 </html>
